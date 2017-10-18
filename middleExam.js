8a009eaeedeb2d668e646117b5a634b4784abced
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-10-18 14:10:15 +0900

    간단한 데이터 조작

---
+++ middleExam.js
@@ -0,0 +1,45 @@
+var widget = {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": {
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+}
+
+
+function checkTypeNum(object) {
+    Object.keys(object).map(function (objectKey, index) {
+        var value = object[objectKey];
+        if (value.toString() === "[object Object]") {
+            checkTypeNum(value);
+        } else if (Number.isInteger(value)) {
+            output.push(objectKey);
+        }
+    });
+}
+
+var output = [];
+checkTypeNum(widget);
+console.log(output);
+
+//실행결과
+//["width", "height", "hOffset", "vOffset", "size", "hOffset", "vOffset"]
