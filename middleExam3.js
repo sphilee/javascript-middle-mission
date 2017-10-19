14fe3b5c147b1d50d58232affcde094866c976df
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-10-19 11:38:44 +0900

    change errMsg to object literal

--- middleExam3.js
+++ middleExam3.js
@@ -15,6 +15,24 @@ function Drink(name, price, amount) {
 
 var drinks = [new Drink("콜라", 1000, 5), new Drink("사이다", 1000, 7), new Drink("포도쥬스", 700, 2), new Drink("딸기우유", 500, 1), new Drink("미에로화이바", 900, 3), new Drink("물", 500, 4), new Drink("파워에이드", 800, 0)];
 
+var errMsgs = {
+    empty: function () {
+        console.log("재고가 없습니다.");
+    },
+    available: function (availableDrink) {
+        console.log("사용가능한 음료수  : " + availableDrink);
+    },
+    select: function (select) {
+        console.log(select + "가 나왔습니다.");
+    },
+    prohibition: function () {
+        console.log("선택할 수 없습니다.");
+    },
+    changes: function (changes) {
+        console.log("잔액은 " + changes + "원입니다.");
+    }
+};
+
 function vendingMachine() {
     rl.question('동전을 넣으세요 : ', function (money) {
         availableDrink(money) ? selectDrink(money) : requestRepurchase(money);
@@ -30,10 +48,10 @@ function availableDrink(money) {
         availableDrink += drink.name + "(" + price + ") ";
     });
     if (availableDrink === '') {
-        console.log("재고 없음");
+        errMsgs.empty();
         return false;
     } else {
-        console.log("사용가능한 음료수  : " + availableDrink);
+        errMsgs.available(availableDrink);
         return true;
     }
 }
@@ -45,18 +63,18 @@ function selectDrink(money) {
         drinks.forEach(function (drink, index) {
             if (drink.name === select) {
                 if (drink.amount < 1) {
-                    console.log("재고가 없습니다.");
+                    errMsgs.empty();
                     selectDrink(money);
                 } else {
                     drink.amount--;
-                    console.log(select + "가 나왔습니다.");
+                    errMsgs.select(select);
                     requestRepurchase(money - drink.price);
                 }
                 check++;
             }
 
             if (check === 0 && (length - 1) === index) {
-                console.log("선택할 수 없습니다.");
+                errMsgs.prohibition();
                 selectDrink(money);
             }
         });
@@ -66,7 +84,7 @@ function selectDrink(money) {
 function requestRepurchase(changes) {
     rl.question('다른걸 구매할까요? 반환할까요? ', function (answer) {
         if (answer === "반환") {
-            console.log("잔액은 " + changes + "원입니다.");
+            errMsgs.changes(changes);
             rl.close();
         } else if (!Number.isNaN(parseInt(answer))) {
             availableDrink(changes) ? selectDrink(changes) : requestRepurchase(changes);
